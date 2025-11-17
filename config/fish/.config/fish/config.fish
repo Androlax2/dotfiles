ed07501063b6df2f7df46cb144c85437734aa767
Androlax2 <theo.benoit16@gmail.com>
2025-11-17 02:14:15 +0100

    docker

--- config/fish/.config/fish/config.fish
+++ config/fish/.config/fish/config.fish
@@ -4,3 +4,10 @@ end
 
 # PHP
 alias sail "bash vendor/bin/sail"
+
+# pnpm
+set -gx PNPM_HOME "/home/theo/.local/share/pnpm"
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
