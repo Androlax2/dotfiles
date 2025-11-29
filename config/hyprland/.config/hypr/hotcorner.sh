94e79fdc71a9ef467c03cf7dc2883d438bdfb432
Androlax2 <theo.benoit16@gmail.com>
2025-11-29 15:35:00 +0100

    steam fullscreen hyprlock

--- config/hyprland/.config/hypr/hotcorner.sh
+++ config/hyprland/.config/hypr/hotcorner.sh
@@ -9,6 +9,18 @@ DEBOUNCE=2     # seconds between triggers
 HEIGHT=$(hyprctl -j monitors | jq '.[0].height')
 
 while true; do
+    # Check if a fullscreen Steam game is running
+    activewindow=$(hyprctl -j activewindow)
+    class=$(echo "$activewindow" | jq -r '.class')
+    fullscreen=$(echo "$activewindow" | jq -r '.fullscreen')
+    
+    # Skip hot corner if Steam or Steam game is fullscreen
+    # fullscreen can be: 0 (no), 1 (maximize), 2 (fullscreen)
+    if [[ "$fullscreen" -ge 1 ]] && [[ "$class" =~ ^(steam|steam_app_) ]]; then
+        sleep 0.5
+        continue
+    fi
+    
     pos=$(hyprctl -j cursorpos)
     x=$(echo "$pos" | jq '.x')
     y=$(echo "$pos" | jq '.y')
